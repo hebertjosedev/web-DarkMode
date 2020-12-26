a71425cd8e5a4af992dd8351650742fd17b0d220
hebert <hebertjosedev@gmail.com>
2020-12-26 17:18:40 -0300

    localstorage

--- js/app.js
+++ js/app.js
@@ -7,4 +7,24 @@ btnSwitch.addEventListener("click", () => {
   btnSwitch.classList.toggle("activee");
   items.classList.toggle("color-letra");
   iconos.classList.toggle("color-letra");
+
+  //Guardamos el modo en localstorage.
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("dark-mode", "true");
+  } else {
+    localStorage.setItem("dark-mode", "false");
+  }
 });
+
+// Obtenemos el modo actual de la pagina web.
+if (localStorage.getItem("dark-mode") == "true") {
+  document.body.classList.add("dark");
+  btnSwitch.classList.toggle("activee");
+  items.classList.add("color-letra");
+  iconos.classList.add("color-letra");
+} else {
+  document.body.classList.remove("dark");
+  btnSwitch.classList.remove("activee");
+  items.classList.remove("color-letra");
+  iconos.classList.remove("color-letra");
+}
